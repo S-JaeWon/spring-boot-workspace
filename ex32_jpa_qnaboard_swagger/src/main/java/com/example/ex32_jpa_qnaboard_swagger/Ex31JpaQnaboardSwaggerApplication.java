a0c863903e586c5bb30c4b14982bfbaa7db7f240
hiide <ssm5170@gmail.com>
2024-11-13 21:12:19 +0900

    feat: swagger

---
+++ ex32_jpa_qnaboard_swagger/src/main/java/com/example/ex32_jpa_qnaboard_swagger/Ex31JpaQnaboardSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex32_jpa_qnaboard_swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex31JpaQnaboardSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex31JpaQnaboardSwaggerApplication.class, args);
+	}
+
+}
